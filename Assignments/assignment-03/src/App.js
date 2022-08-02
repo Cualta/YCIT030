730b10fe7aa94bdafe25f75e12227048329e97b0
cualta <dg.claudiahernandez@gmail.com>
2022-08-01 21:08:16 -0400

    display products page

--- Assignments/assignment-03/src/App.js
+++ Assignments/assignment-03/src/App.js
@@ -4,7 +4,7 @@ import { About } from "./components/About";
 import { Cart } from "./components/Cart";
 import { Home } from "./components/Home";
 import { Navbar } from "./components/Navbar";
-import { ProductsList } from "./components/ProductsList";
+import { Products } from "./components/Products";
 
 export function App() {
   return (
@@ -14,7 +14,7 @@ export function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="about" element={<About />} />
-          <Route path="products" element={<ProductsList />}>
+          <Route path="products" element={<Products />}>
             <Route path="info" element={<>ContactInfo</>} />
             <Route path="media" element={<>ContactSocialMedia</>} />
           </Route>
--- Assignments/assignment-03/src/components/ProductsList.js
+++ Assignments/assignment-03/src/components/ProductsList.js
@@ -5,26 +5,28 @@ export function ProductsList(props) {
   const arraychildren = Children.toArray(props.children);
 
   return (
-    <Grid
-      display="grid"
-      gridTemplateRows="repeat(4, 1fr)"
-      gridTemplateColumns={"repeat(3, 1fr)"}
-      gap={5}
-      component="products"
-    >
-      {arraychildren.map((child) => {
-        return (
-          <Grid
-            item
-            key={child.key}
-            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-            component="card"
-            sx={{ textAlign: "center" }}
-          >
-            {child}
-          </Grid>
-        );
-      })}
-    </Grid>
+    <main>
+      <Grid
+        display="grid"
+        gridTemplateRows="repeat(4, 1fr)"
+        gridTemplateColumns={"repeat(3, 1fr)"}
+        gap={3}
+        component="products"
+      >
+        {arraychildren.map((child) => {
+          return (
+            <Grid
+              item
+              key={child.key}
+              columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+              component="card"
+              sx={{ textAlign: "center" }}
+            >
+              {child}
+            </Grid>
+          );
+        })}
+      </Grid>
+    </main>
   );
 }
